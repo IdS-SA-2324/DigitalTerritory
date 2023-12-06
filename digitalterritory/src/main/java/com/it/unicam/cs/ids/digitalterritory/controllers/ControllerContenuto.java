abf8fe3ef020fa430d702c0d377e1da11c1cd178
paulRagazziniUnicam <paulluc.savragazzini@studenti.unicam.it>
2023-12-06 18:44:46 +0100

    Aggiunto progetto java con package e classi base

---
+++ digitalterritory/src/main/java/com/it/unicam/cs/ids/digitalterritory/controllers/ControllerContenuto.java
@@ -0,0 +1,6 @@
+package com.it.unicam.cs.ids.digitalterritory.controllers;
+
+public class ControllerContenuto {
+    public Object getContenuti(){return null;}
+
+}
---
+++ digitalterritory/src/main/java/com/it/unicam/cs/ids/digitalterritory/utils/Coordinate.java
@@ -0,0 +1,4 @@
+package com.it.unicam.cs.ids.digitalterritory.utils;
+
+public record Coordinate(double x, double y, double z) {
+}
