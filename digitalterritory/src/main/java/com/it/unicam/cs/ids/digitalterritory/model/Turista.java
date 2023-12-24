4697d983fab4d54ecf81c3e1c83c3e400ce9ffb5
paulRagazziniUnicam <paulluc.savragazzini@studenti.unicam.it>
2023-12-24 11:11:55 +0100

    Aggiunto stack modello-codice per use case Salva Contenuti

--- digitalterritory/src/main/java/com/it/unicam/cs/ids/digitalterritory/model/Turista.java
+++ digitalterritory/src/main/java/com/it/unicam/cs/ids/digitalterritory/model/Turista.java
@@ -1,5 +1,21 @@
 package com.it.unicam.cs.ids.digitalterritory.model;
 
+import com.it.unicam.cs.ids.digitalterritory.controllers.DBMSTouristSearcher;
+
+
 public class Turista {
+    private boolean exitFlag;
     public void creaSegnalazione(){}
+
+    public void visualizzaContenuti(Comune comune){
+        DBMSTouristSearcher personalSearcehr = new DBMSTouristSearcher();
+        personalSearcehr.getPuntiInteresse(comune);
+        while (!exitFlag){
+            //il turista scorre i contenuti 
+        }
+    }
+
+    public void exit(){
+        this.exitFlag=true;
+    }
 }
